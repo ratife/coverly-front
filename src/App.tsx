39802a82fcd9de6bcf5e7a757f0b03f62963ff5f
Eugene Ralaivao <eugene.ralaivao@kosmopolead.com>
2026-02-14 11:31:36 +0300

    [FIX] change place view

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
-import AdOverlayPricingPage from './pages/adoverlay-pricing-page/AdOverlayPricingPage';
-import AnnonceurCampaignPage from './pages/annonceur/annonceur-campaign-manager/AnnonceurCampaignPage';
-import CoverAdsMonetizePage from './pages/coverads-monetize-every-view/CoverAdsMonetizePage';
+import AdOverlayPricingPage from './pages/audience/adoverlay-pricing-page/AdOverlayPricingPage';
+import AnnonceurCampaignPage from './pages/advertiser/annonceur-campaign-manager/AnnonceurCampaignPage';
+import CoverAdsMonetizePage from './pages/audience/home/CoverAdsMonetizePage';
 import EspaceCreateurPage from './pages/publisher/espace-createur-dashboard/EspaceCreateurPage';
-import AdCoverProPage from './pages/adcover-pro/AdCoverProPage';
-import AdOverlayMarketPage from './pages/adoverlay-market/AdOverlayMarketPage';
-import CreatorConnectPage from './pages/creatorconnect/CreatorConnectPage';
-import AdvisionProEditorPage from './pages/advision-pro-editor/AdvisionProEditorPage';
-import EspaceCreateurPortfolioPage from './pages/espace-createur-portfolio/EspaceCreateurPortfolioPage';
-import EspaceCreateurMarketplacePage from './pages/espace-createur-marketplace/EspaceCreateurMarketplacePage';
+import AdCoverProPage from './pages/advertiser/adcover-pro/AdCoverProPage';
+import AdOverlayMarketPage from './pages/advertiser/adoverlay-market/AdOverlayMarketPage';
+import CreatorConnectPage from './pages/publisher/creatorconnect/CreatorConnectPage';
+import AdvisionProEditorPage from './pages/publisher/advision-pro-editor/AdvisionProEditorPage';
+import EspaceCreateurPortfolioPage from './pages/publisher/espace-createur-portfolio/EspaceCreateurPortfolioPage';
+import EspaceCreateurMarketplacePage from './pages/advertiser/espace-createur-marketplace/EspaceCreateurMarketplacePage';
 import './App.css';
 import HeaderStandard from "./layouts/HeaderStandard";
 
